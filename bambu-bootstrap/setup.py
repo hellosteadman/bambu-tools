7d3b700b4c05bdf2360a83bd3ce64b3ef6f1d96c
Mark Steadman <marksteadman@me.com>
2014-01-12 15:04:21 +0000

    Updated bambu-bootstrap version number

--- bambu-bootstrap/setup.py
+++ bambu-bootstrap/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name = 'bambu-bootstrap',
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'Use Twitter\'s Bootstrap CSS framework to build your app. All the views Bambu uses all extend a base template which you create, that can be based on a skeleton Bootstrap template. Shortcut tags let you easily add breadcrumb trails and icons to your apps.',
 	author = 'Steadman',
 	author_email = 'mark@steadman.io',
@@ -12,18 +12,31 @@ setup(
 	packages = [
 		'bambu',
 		'bambu.bootstrap',
-		'bambu.bootstrap.templatetags'
+		'bambu.bootstrap.templatetags',
+		'bambu.bootstrap.v2',
+		'bambu.bootstrap.v2.templatetags',
+		'bambu.bootstrap.v3',
+		'bambu.bootstrap.v3.templatetags'
 	],
 	package_data = {
-		'bambu.bootstrap': [
+		'bambu.bootstrap.v2': [
 			'static/bootstrap/css/*.css',
 			'static/bootstrap/css/jquery-ui/*.css',
 			'static/bootstrap/css/jquery-ui/images/*.png',
 			'static/bootstrap/js/*.js',
 			'static/bootstrap/img/*.png',
 			'static/bootstrap/font/*.*',
-			'static/bootstrap/3.0/css/*.css',
-			'static/bootstrap/3.0/js/*.js',
+			'templates/bootstrap/*.html',
+			'templates/search/*.html',
+			'templates/*.html'
+		],
+		'bambu.bootstrap.v3': [
+			'static/bootstrap/css/*.css',
+			'static/bootstrap/css/jquery-ui/*.css',
+			'static/bootstrap/css/jquery-ui/images/*.png',
+			'static/bootstrap/js/*.js',
+			'static/bootstrap/img/*.png',
+			'static/bootstrap/font/*.*',
 			'templates/bootstrap/*.html',
 			'templates/search/*.html',
 			'templates/*.html'
