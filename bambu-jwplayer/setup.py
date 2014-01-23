ac73ae6cfc1fe21c067f7cdb1967bec3f6305113
Mark Steadman <marksteadman@me.com>
2014-01-23 12:34:20 +0000

    Updated bambu.jwplayer version number

--- bambu-jwplayer/setup.py
+++ bambu-jwplayer/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name = 'bambu-jwplayer',
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'A wrapper around the JWPlayer',
 	author = 'Steadman',
 	author_email = 'mark@steadman.io',
