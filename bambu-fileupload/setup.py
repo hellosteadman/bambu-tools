4eab942a368bd4821c00ba5b577fb93fb562aaee
Steadman <mark+github@steadman.io>
2014-01-27 14:16:32 +0000

    Changed file-upload script to use enclosure method

--- bambu-fileupload/setup.py
+++ bambu-fileupload/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
 	name = 'bambu-fileupload',
-	version = '0.0.3',
+	version = '0.1',
 	description = 'A wrapper around the jQuery.fileupload library',
 	author = 'Steadman',
 	author_email = 'mark@steadman.io',
