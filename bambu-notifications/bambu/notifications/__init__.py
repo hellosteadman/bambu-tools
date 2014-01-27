9d7ac2f2c149b357c3fd3f5495345f8813931de6
Steadman <mark+github@steadman.io>
2014-01-27 15:31:14 +0000

    Updated notifications styling

--- bambu-notifications/bambu/notifications/__init__.py
+++ bambu-notifications/bambu/notifications/__init__.py
@@ -7,7 +7,7 @@ from django.utils.importlib import import_module
 from bambu.notifications.models import Notification
 from bambu.notifications.options import NotificationTemplate
 
-__version__ = '0.0.2'
+__version__ = '0.1'
 
 try:
 	import json as simplejson
