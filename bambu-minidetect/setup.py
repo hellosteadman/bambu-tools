b80cc18c97cc1527ebc19f45992b3e134786ea71
Steadman <mark+github@steadman.io>
2014-01-28 15:59:33 +0000

    Added namespace package reference to minidetect

--- bambu-minidetect/setup.py
+++ bambu-minidetect/setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(
 	name = 'bambu-minidetect',
-	version = '0.0.1',
+	version = '0.1',
 	description = 'Tools for Django webapps',
 	author = 'Steadman',
 	author_email = 'mark@steadman.io',
 	url = 'http://pypi.python.org/pypi/bambu-minidetect',
 	install_requires = ['Django>=1.4'],
+	namespace_packages = ['bambu'],
 	packages = [
-		'bambu',
 		'bambu.minidetect'
 	],
 	package_data = {
