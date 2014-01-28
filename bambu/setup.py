2db68b772de1555ffc482b0111bf162777fd612c
Steadman <mark+github@steadman.io>
2014-01-28 16:32:11 +0000

    Updated description for base package

--- bambu/setup.py
+++ bambu/setup.py
@@ -4,16 +4,10 @@ from distutils.core import setup
 setup(
     name = 'bambu',
     version = '0.1',
-    description = 'Base module for bambu-tools',
+    description = 'Namespace package for the Bambu Tools toolset',
     author = 'Steadman',
     author_email = 'mark@steadman.io',
     url = 'http://pypi.python.org/pypi/bambu-analytics',
-    install_requires = [
-        'bambu',
-        'Django>=1.4',
-        'oauth',
-        'oauth2'
-    ],
     packages = ['bambu'],
     classifiers = [
         'Development Status :: 4 - Beta',
