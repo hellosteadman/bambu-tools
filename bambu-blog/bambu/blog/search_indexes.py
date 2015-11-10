3c790a68e4d9b5baeb8d2b272f20d962bd4be353
Mark Steadman <mark@steadman.io>
2015-11-10 16:14:58 +0000

    Updated Haystack index in Blog

--- bambu-blog/bambu/blog/search_indexes.py
+++ bambu-blog/bambu/blog/search_indexes.py
@@ -1,16 +1,14 @@
-from haystack import indexes, site
+from haystack import indexes
 from bambu.blog.models import Post
 from datetime import datetime
 
-class PostIndex(indexes.SearchIndex):
+class PostIndex(indexes.SearchIndex, indexes.Indexable):
 	text = indexes.CharField(document = True, use_template = True)
 	author = indexes.CharField(model_attr = 'author')
 	date = indexes.DateTimeField(model_attr = 'date')
-	
+
 	def get_model(self):
 		return Post
-	
+
 	def index_queryset(self, using = None):
 		return super(PostIndex, self).index_queryset().live()
-
-site.register(Post, PostIndex)
